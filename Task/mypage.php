7f7a4b543f5decbeee519dc2afe5de576d072d51
Shamit Nibras <19-40117-1@student.aiub.edu>
2021-05-31 19:46:32 +0600

    first commit

---
+++ Task/mypage.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<htitle>My Home Page</title>
+</head>
+<body>
+<h1>My First Page</h1>
+<marguee><h2>My Profile</h2></marguee>
+<a href- "https://www.w3schools.com" >Visit w3schools</a>
+
+<form>
+First Name: <input type="text" name="fname"><br>
+last Name: <input type="text"> <br>
+Choose one option <br>
+Male <input type="radio" name="gender">
+Female <input type="radio" name="gender">
+<br>
+Music <input type="radio" name="hobby">
+Dance <input type="radio" name="hobby">
+<br>
+<input type="date"><br>
+<input type="submit" value ="submit">
+<input type="reset">
+</form>
+</body>
+</html>
